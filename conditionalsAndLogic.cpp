df8c1d209bd0d147e78e1cccf2b9067e4553fe6e
ranepours <ranejdavis@gmail.com>
2022-06-06 18:10:40 -0400

    yup

--- conditionalsAndLogic.cpp
+++ conditionalsAndLogic.cpp
@@ -1,7 +1,8 @@
 //if, else, else if & switch statements => set up looks to be the same as js
 //relational and logical operators
-
 #include <iostream>
+#include <cstdlib>
+
 int main(){
     int grade = 90;
     if (grade > 60){
@@ -90,4 +91,60 @@ int main(){
     }
 
     std::cout << "Your weight: " << weight << "\n";
+
+//can use keywords in place of symbols
+/* != => not, && => and, || => or*/
+    int year;
+
+    std::cout << "Enter a year:\n";
+    std::cin >> year;
+
+    if(year < 1000 || year > 9999){
+        std::cout << "Invalid entry.\n";
+    } else if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
+        std::cout << "You found a leap year: " << year << "\n";
+    } else {
+        std::cout << "Not a leap year: " << year << "\n";
+    }
+
+//magic-8
+    std::cout << "Magic 8-Ball\n\n";
+    srand(time(NULL)); //sets "seed" of random # generator
+    int answer = std::rand() % 10; //randomizes numbers 0-9
+    std::cout << answer;
+    switch(answer){
+        case 0:
+            std::cout << "\n It is certain";
+            break;
+        case 1:
+            std::cout << "\n Concentrate and ask again";
+            break;
+        case 2:
+            std::cout << "\n As I see it, yes";
+            break;
+        case 3:
+            std::cout << "\n Don't count on it";
+            break;
+        case 4:
+            std::cout << "\n Reply hazy, try again";
+            break;
+        case 5:
+            std::cout << "\n Better not tell you now";
+            break;
+        case 6:
+            std::cout << "\n Outlook not so good";
+            break;
+        case 7:
+            std::cout << "\n My sources say no";
+            break;
+        case 8:
+            std::cout << "\n Outlook good";
+            break;
+        case 9:
+            std::cout << "\n Very doubtful";
+            break;
+        default:
+            std::cout << "\n Nothing to report";
+            break;
+    }
 }
